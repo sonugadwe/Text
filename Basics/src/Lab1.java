a31aef3b0792f66b1b5b03c14e6a9996f1b6ceb3
sonu <sonu@LAPTOP-5E3MODR9>
2019-10-04 16:23:16 +0530

    first commit

---
+++ Basics/src/Lab1.java
@@ -0,0 +1,16 @@
+
+public class Lab1 {
+	
+	String s1="hello";
+	String s2="hi";
+	String s3=" ";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int $b=20;
+		System.out.println(-a);
+		System.out.println($b);
+	}
+
+}
